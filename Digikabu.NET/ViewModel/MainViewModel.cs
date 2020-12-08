4e9e095463a7263a9d1988a18462512d94dd14e6
Julian <julianb2703@gmail.com>
2020-12-09 00:28:34 +0100

    managed Messageboxevent in LoginWindow

--- Digikabu.NET/ViewModel/MainViewModel.cs
+++ Digikabu.NET/ViewModel/MainViewModel.cs
@@ -39,9 +39,19 @@ namespace Digikabu.NET
 
         public void OnLoginClick(object obj)
         {
-
+            RequestMessageBox(null, Name, "demoCaption", false);
         }
 
         #endregion
+
+        private void RequestMessageBox(Action<bool> resultAction, string messageBoxText, string caption = "", bool onlyOk = true)
+        {
+            this.MessageBoxRequest?.Invoke(this, new MvvmMessageBoxEventArgs(resultAction, messageBoxText, caption, onlyOk));
+        }
+
+        private void RequestDialog(DialogType type, Action actionAfterCreate = null)
+        {
+            this.DialogCreateRequest?.Invoke(this, new MvvmDialogEventArgs(type, actionAfterCreate));
+        }
     }
 }
