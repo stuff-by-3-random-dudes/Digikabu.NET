d7734b228c4e1621e63279be36fe4fd7f8597d03
Julian <julianb2703@gmail.com>
2020-12-09 00:07:45 +0100

    Connected ViewModel with gui

---
+++ Digikabu.NET/Model/DigikabuException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Digikabu.NET.Model
+{
+    class DigikabuException : Exception
+    {
+        public DigikabuException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- Digikabu.NET/ViewModel/MainViewModel.cs
+++ Digikabu.NET/ViewModel/MainViewModel.cs
@@ -1,7 +1,47 @@
-﻿namespace Digikabu.NET.ViewModel
+﻿using System;
+using System.Windows.Input;
+
+namespace Digikabu.NET
 {
     class MainViewModel : BaseModel
     {
+        #region Events
 
+        public event EventHandler<MvvmMessageBoxEventArgs> MessageBoxRequest;
+        public event EventHandler<MvvmDialogEventArgs> DialogCreateRequest;
+        public event EventHandler<EventArgs> DialogCloseRequest;
+
+        #endregion
+
+        #region Databinding
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OnLoginClickCommand { get; set; }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            SetupCommands();
+        }
+
+        public void SetupCommands()
+        {
+            OnLoginClickCommand = new ActionCommand(OnLoginClick);
+        }
+
+        #region Mvvm Methods
+
+        public void OnLoginClick(object obj)
+        {
+
+        }
+
+        #endregion
     }
 }
